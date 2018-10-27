63eb20f6b8f5b66aeaaac78eb15f13f248a4687c
helio <helio_dantas@outlook.com>
2018-10-27 17:44:22 -0300

    hh

--- Lancamento/src/main/java/br/net/smi/lancamento/config/Handle.java
+++ Lancamento/src/main/java/br/net/smi/lancamento/config/Handle.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolverComposite;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.net.smi.lancamento.model.Erro;
@@ -40,7 +43,18 @@ public class Handle extends ResponseEntityExceptionHandler {
 		novoErro.setTitle("Entrada invalida");
 		return handleExceptionInternal(rn,novoErro,  new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
-
+	
+	protected ResponseEntity<Object> HandlerMethodArgumentResolverComposite(MethodArgumentNotValidException rn,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		Erro novoErro = new Erro();
+		novoErro.setDetail(rn.getMessage());
+		novoErro.setDevMensagem(rn.getClass().getName());
+		novoErro.setStatus(HttpStatus.NOT_FOUND.value());
+		novoErro.setTimeStamp(LocalDate.now());
+		novoErro.setTitle("Entrada invalida");
+		return handleExceptionInternal(rn,novoErro,  new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+	}
+	
 	
 	@ExceptionHandler(value = { IllegalArgumentException.class, IllegalStateException.class })
 	protected ResponseEntity<Object> handleConflict(RuntimeException rn, WebRequest request) {
@@ -50,7 +64,7 @@ public class Handle extends ResponseEntityExceptionHandler {
 		novoErro.setDevMensagem(rn.getClass().getName());
 		novoErro.setStatus(HttpStatus.NOT_FOUND.value());
 		novoErro.setTimeStamp(LocalDate.now());
-		novoErro.setTitle("Entrada invalida");
+		novoErro.setTitle("Entrada ");
 		return handleExceptionInternal(rn, novoErro, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 }
